4a22a6824f857cfe55f7f2fa9f2dfd694912770c
alirezaalavi7713 <alirezaalavi7713@gmail.com>
2025-06-23 15:51:31 +0200

    model

--- index.js
+++ index.js
@@ -11,20 +11,18 @@ class voiture {
     this.#couleur = couleur;
     }
 
-    get model(){
-        return this.#model;
-    }
-    set model(modelrecu){
-        if(modelrecu.length > 0)
-            this.#model = modelrecu
-    }
+    get model(){return this.#model;}
+    set model(modelrecu){this.#model = modelrecu}
 
     afficherdetails(){
         console.log(`ma voiture est une ${this.#marque},${this.#model},de l'annee ${this.#Annee},avec la couleur ${this.#couleur}`)
     }
 }
 
-const maVoiture = new voiture("opel","passat",2024,"blanc")
+const maVoiture = new voiture("opel","suv",2024,"blanc")
 maVoiture.model = "astra"
+const maVoiture2 = new voiture("volkswagen","passat",2006,"gris")
+maVoiture.model = "golf6"
 
 maVoiture.afficherdetails()
+maVoiture2.afficherdetails()
